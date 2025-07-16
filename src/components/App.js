e3a95f6c2e17842b35c91372c36fb0700499fd48
Gunjan Singh <gunjansingh72002@gmail.com>
2025-07-16 14:04:31 +0530

    golf-game completed

--- src/components/App.js
+++ src/components/App.js
@@ -6,27 +6,48 @@ class App extends Component {
         super(props)
         this.state = {
             renderBall: false,
-            posi : 0,
-            ballPosition: { left: "0px" }
+            posi: 0,
+            ballPosition: { left: "0px", position : "absolute" }
         };
         this.renderChoice = this.renderBallOrButton.bind(this)
         this.buttonClickHandler = this.buttonClickHandler.bind(this)
+        this.handleKeyDown = this.handleKeyDown.bind(this)
     };
 
-    buttonClickHandler() {
    
-   }
+
+    handleKeyDown(e) {
+        if (e.key == "ArrowRight") {
+            this.setState((prevState) => {
+                const newPosition = prevState.posi + 5;
+                return {
+                    posi: newPosition,
+                    ballPosition: { left: newPosition + "px" }
+                }
+            })
+        }
+    }
+
+
+    buttonClickHandler() {
+        this.setState({ renderBall: true })
+    }
+
     renderBallOrButton() {
-		if (this.state.renderBall) {
-		    return <div className="ball" style={this.state.ballPosition}></div>
-		} else {
-		    return <button onClick={this.buttonClickHandler} >Start</button>
-		}
+        if (this.state.renderBall) {
+            return <div id = "ball" className="ball" style={{...this.state.ballPosition, position : "absolute"}}></div>
+        } else {
+            return <button className="start" onClick={this.buttonClickHandler} >Start</button>
+        }
     }
 
     // bind ArrowRight keydown event
     componentDidMount() {
-      
+        document.addEventListener('keydown', this.handleKeyDown)
+    }
+
+    componentWillUnmount() {
+        document.removeEventListener('keydown', this.handleKeyDown)
     }
 
     render() {
